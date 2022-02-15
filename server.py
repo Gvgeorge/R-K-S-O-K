7ccf233c0dd3727a7b0251b6808f6e884932840e
gvgeorge <gvgeorge@mail.ru>
2022-02-15 16:46:09 +0300

    more changes to documentation

--- server.py
+++ server.py
@@ -10,24 +10,38 @@ from exceptions import RKSOKException, NameIsTooLongError, \
 
 
 class FilePhoneBook:
-    def __init__(self, folder_path):
+    '''
+    Class for storing phones in the file system
+    Creates a separate file for each recorded name
+    '''
+    def __init__(self, folder_path: str) -> None:
         self.folder_path = folder_path
         if not os.path.exists(self.folder_path):
             os.makedirs(self.folder_path)
 
-    async def get(self, name):
+    async def get(self, name: str) -> str:
+        '''
+        Returns the contents of the file of a given name
+        '''
         file_path = os.path.join(self.folder_path, name)
         async with aiofiles.open(file_path, mode='r') as f:
             contents = await f.readlines()
         return '\r\n'.join(line.strip() for line in contents)
 
-    async def write(self, name, phones):
+    async def write(self, name: str, phones: list) -> None:
+        '''
+        Writes phones from to the file of a given name
+        Each call of this function rewrites the file
+        '''
         file_path = os.path.join(self.folder_path, name)
         async with aiofiles.open(file_path, mode='w') as f:
             phones = [phone.strip() for phone in phones if phone.strip()]
             await f.writelines([f'{phone}\r\n' for phone in phones])
 
-    def delete(self, name):
+    def delete(self, name: str) -> None:
+        '''
+        Deletes the file of a given name from the filesystem
+        '''
         os.remove(os.path.join(self.folder_path, name))
 
 
